897514e077f3f69007d9a433cc6d1f67634623be
Helena <mhas@cin.ufpe.br>
2018-07-07 18:51:16 -0300

    add bunkers

--- probatus_est_bot.py
+++ probatus_est_bot.py
@@ -7,6 +7,9 @@ from sc2.player import Bot, Computer
 from sc2.player import Human
 
 class ProbatusEstBot(sc2.BotAI):
+    
+    enemy_position = 0
+
     def select_target(self):
         target = self.known_enemy_structures
         if target.exists:
@@ -30,6 +33,13 @@ class ProbatusEstBot(sc2.BotAI):
             return
         else:
             cc = cc.first
+            
+#fechar a base (construir bunker)
+        if self.can_afford(BUNKER) and self.units(BUNKER).amount < 3:
+            if self.enemy_position == 0:
+                await self.build(BUNKER, near=cc.position.towards(self.game_info.map_center, 50))
+            else:
+                await self.build(BUNKER, near=cc.position.towards(self.enemy_position, 50))
 
 #attack
         if iteration % 50 == 0 and (self.units(HELLIONTANK).amount > 2 or self.units(MARINE) ):
@@ -113,7 +123,7 @@ def main():
     sc2.run_game(sc2.maps.get("Sequencer LE"), [
         # Human(Race.Terran),
         Bot(Race.Terran, ProbatusEstBot()),
-        Computer(Race.Zerg, Difficulty.Easy)
+        Computer(Race.Zerg, Difficulty.Hard)
     ], realtime=False)
 
 if __name__ == '__main__':
